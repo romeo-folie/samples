816a0ffa3fb14e43ef4cc1b7d2b971030cc49301
Romeo Folie <romeo.folie@bewsys.com>
2021-04-13 06:38:56 +0000

    updated fxn exp keywords

--- samples.js
+++ samples.js
@@ -6,31 +6,31 @@
 
 const sampleArray = [2,3,4,5,6,7]
 
-let first_or_first_n = function(theArray, n){
+const first_or_first_n = function(theArray, n){
     // get first n elements of the array
     if(n > 0 && n <= theArray.length) {
         let resArray = []
 
         for(let i = 0; i < n; i++){
-            resArray.push(theArray[i]);
+            resArray.push(theArray[i])
         }
 
         return resArray;
     }
     // return the first element of the array
-    n = 0;
+    n = 0
     return [theArray[n]];
 }
 
 console.log(first_or_first_n(sampleArray, 3))
 
-let last_or_last_n = function(theArray, n){
+const last_or_last_n = function(theArray, n){
     if(n > 0 && n <= theArray.length){
         return theArray.slice(-n)
     }
     //return last element of the array
     n = theArray.length - 1
-    return [theArray[n]];
+    return [theArray[n]]
 }
 
-console.log(last_or_last_n(sampleArray, 2));
+console.log(last_or_last_n(sampleArray, 2))
