1813236b3e9bfa9a51fcd4a38a8371f43f811ff7
Romeo Folie <romeo.folie@bewsys.com>
2021-04-14 09:01:58 +0000

    added solution to problem three

---
+++ problem-three.js
@@ -0,0 +1,28 @@
+/*
+    Write a javascript function that accepts two arrays and merges them to produce an interleaved output
+
+    E.g
+
+    var A = [1, 3, 5, 7, ‘c’]
+    var B = [‘a’, 4, 6, ‘b’, 10, 12, 14]
+
+    Output  = [1, ‘a’, 3, 4, 5, 6, 7, ‘b’, ‘c’, 10, 12, 14]
+*/
+
+const mergeArrays = function (array1, array2){
+    let resArray = [];
+    
+    const arrayLength = array1.length > array2.length ? array1.length : array2.length
+
+    for(let i = 0; i < arrayLength; i++){
+        if(array1[i]) resArray.push(array1[i])
+        if(array2[i]) resArray.push(array2[i])
+    }
+
+    return resArray;
+}
+
+const A = [1, 3, 5, 7, 'c']
+const B = ['a', 4, 6, 'b', 10, 12, 14]
+console.log(mergeArrays(A,B))
+
